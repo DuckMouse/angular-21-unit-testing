9148df448aed959515c40f7cf81efe9885b25d57
Auret Swanepoel <auret.swanepoel@practiceevolve.com.au>
2026-01-11 15:44:50 +1100

    remove exlude array

--- Module 4/report-coverage/vitest-base.config.ts
+++ Module 4/report-coverage/vitest-base.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // ...
-    // include: [
-    //   'src/**/ *.spec.ts',
-    // ],
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
